b0497538fb6cfe12cd4b118aa8887f3446558ffc
Shaun Ritty <shaunritty@gmail.com>
2023-11-03 03:18:50 -0700

    chat interface completed

--- app/src/main/java/com/example/gympartner/ChatActivity.java
+++ app/src/main/java/com/example/gympartner/ChatActivity.java
@@ -1,8 +1,11 @@
 package com.example.gympartner;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -11,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
@@ -18,10 +22,15 @@ import android.widget.Toolbar;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -36,6 +45,11 @@ public class ChatActivity extends AppCompatActivity {
     private EditText messageInputText;
 
     private DatabaseReference RootRef;
+    private final List<Messages> messagesList=new ArrayList<>();
+    private LinearLayoutManager linearLayoutManager;
+    private MessageAdapter messageAdapter;
+
+    private RecyclerView userMessagesList;
 
     private FirebaseAuth mAuth;
 
@@ -86,11 +100,49 @@ public class ChatActivity extends AppCompatActivity {
         sendMessageButton=(ImageButton) findViewById(R.id.send_message_btn);
         messageInputText=(EditText) findViewById(R.id.input_message);
 
-
+        messageAdapter=new MessageAdapter(messagesList);
+        userMessagesList=(RecyclerView) findViewById(R.id.private_messages_list_of_users);
+        linearLayoutManager=new LinearLayoutManager(this);
+        userMessagesList.setLayoutManager(linearLayoutManager);
+        userMessagesList.setAdapter(messageAdapter);
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        RootRef.child("Messages").child(messageSenderId).child(messageReceiverId).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Messages messages=snapshot.getValue(Messages.class);
+                messagesList.add(messages);
+                messageAdapter.notifyDataSetChanged();
 
+                userMessagesList.smoothScrollToPosition(userMessagesList.getAdapter().getItemCount());
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
 
     private void SendMessage(){
 
